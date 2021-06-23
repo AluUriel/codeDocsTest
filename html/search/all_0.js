32c70e571e12bc73a96fd25123ea005090459a4b
AluUriel <alu.uriel.gp@gmail.com>
2021-06-23 03:40:50 -0500

    third commit

---
+++ html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['foo_0',['Foo',['../class_foo.html',1,'']]],
+  ['foo2_1',['Foo2',['../class_foo2.html',1,'']]]
+];
---
+++ html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['foo_2',['Foo',['../class_foo.html',1,'']]],
+  ['foo2_3',['Foo2',['../class_foo2.html',1,'']]]
+];
