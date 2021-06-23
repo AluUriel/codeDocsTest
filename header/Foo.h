e1b5d38efde44e6a484f53341b70293de23ede1d
AluUriel <alu.uriel.gp@gmail.com>
2021-06-23 03:34:50 -0500

    second commit

---
+++ header/Foo.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+/// Foo
+class Foo {
+
+    /// Say hello
+    /// @param message The message to print
+    void say_hello(std::string message) const;
+
+};
+
+/// Foo
+class Foo2 {
+
+    /// Say hello
+    /// @param message The message to print
+    void say_hello(std::string message) const;
+
+};
